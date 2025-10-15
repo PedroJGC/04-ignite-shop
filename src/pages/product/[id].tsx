7ff8e72c8a12343f42289fa8131f88ce7116002f
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-15 16:30:07 -0300

    feat: implement product page layout with styled components for better structure and styling

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo rerum
+          laborum corrupti iste commodi cum consequuntur aspernatur, natus quasi
+          voluptate quaerat blanditiis, voluptatum odit omnis corporis excepturi
+          debitis, eligendi quam?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
